2ce5ca8a2f663624315befe60a8e561d86ebf339
Rammina <49326578+Rammina@users.noreply.github.com>
2021-07-03 14:28:31 +0800

    Update projects.js

--- server/routes/projects.js
+++ server/routes/projects.js
@@ -46,7 +46,6 @@ router.post("/:id/create-checkout-session", async (req, res) => {
       userId ||
       Math.random().toString(36).substring(7) + projectId + projectCreatorId,
     metadata: { projectId, projectCreatorId, userId, amount },
-    // note: replace for production environment
     success_url: `https://resto-fund.netlify.app/projects/${projectId}?checkoutStatus=success`,
     cancel_url: `https://resto-fund.netlify.app/projects/${projectId}?checkoutStatus=canceled`,
   });
